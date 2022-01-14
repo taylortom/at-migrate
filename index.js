f6231773ad007668b7d521ea588f07406fdb8fee
Tom Taylor <hello@tomtaylor.name>
2022-01-14 13:18:54 +0000

    Add new migration process

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const migrate = require('./migrate');
+import Exporter from "./lib/Exporter.cjs";
+import Importer from "./lib/Importer.js";
 
-module.exports = {
-  migrate
-};
+async function run() {
+  try {
+    const oldDir = '/home/tom/Projects/adapt/adapt_authoring';
+    const newDir = '/home/tom/Projects/1_scratch/adapt-authoring';
+
+    const exporter = new Exporter(oldDir);
+    await exporter.init();
+    await exporter.run();
+
+    const importer = new Importer(exporter.exportPath, newDir);
+    await importer.init();
+    const importData = await importer.run();
+
+    if(importData.fail) {
+      console.log(importData.fail);
+    }
+  } catch(e) {
+    console.log(e);
+  }
+  // @todo cleanup
+  process.exit();
+}
+
+export default run();
