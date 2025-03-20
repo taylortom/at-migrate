fca033121da4f1e9a7ec42d4e94e5299d685029c
Tom Taylor <hello@tomtaylor.name>
2025-03-20 00:45:04 +0000

    Updates

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ import Exporter from "./lib/Exporter.cjs";
 import Importer from "./lib/Importer.js";
 import prompts from 'prompts';
 
+function getArgs() {
+  const [
+    action
+  ] = process.argv.slice(2);
+  return {
+    action
+  };
+}
+
 async function run() {
+  const {  
+    action
+  } = getArgs();
   const errors = [];
   let app;
   try {
+    console.log(action);
     const options = await getInput();
-    const exporter = new Exporter(options);
-    await exporter.init();
-    const { courses, roles, users, exportPath } = await exporter.run();
-
-    options.courses = courses;
-    options.oldRoles = roles;
-    options.oldUsers = users;
-    options.exportPath = exportPath;
-
-    const importer = new Importer(options);
-    await importer.init();
-    app = importer.app;
-    const { success, fail } = await importer.run();
-
-    if(fail) errors.push(...(fail instanceof Error ? [fail] : Object.values(fail)));
+    
+    switch(action) {
+      case 'export':
+        await runExport(options);
+        break;
+      case 'import':
+        await runImport(options);
+        break;
+      default:
+        throw new Error('Invalid action');
+    }
   } catch(e) {
     errors.push(e);
   }
@@ -34,7 +43,32 @@ async function run() {
   process.exit();
 }
 
+async function runExport(options) {
+  const exporter = new Exporter(options);
+  await exporter.init();
+  return exporter.run();
+}
+
+async function runImport(options) {
+  const importer = new Importer({
+    ...options,
+    courses: courses,
+    oldRoles: roles,
+    oldUsers: users,
+    exportPath: exportPath
+  });
+  await importer.init();
+  app = importer.app;
+  const { success, fail } = await importer.run();
+  if(fail) errors.push(...(fail instanceof Error ? [fail] : Object.values(fail)));
+}
+
 async function getInput() {
+  return {
+    oldToolPath: process.cwd(),
+    newToolPath: process.cwd(),
+    forceRebuild: true
+  };
   try {
     return await prompts([{
       type: 'text',
